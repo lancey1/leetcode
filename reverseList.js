55b69ebd5faf0c65cb321f8e996610cb4b82713c
Lance Yu <lanceyu619@gmail.com>
2023-05-11 15:30:35 -0400

    linked list

---
+++ reverseList.js
@@ -0,0 +1,32 @@
+function reverseList(head) {
+  let prev = null;
+  let current = head;
+
+  while (current !== null) {
+    let next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+
+  return prev;
+}
+
+
+const linkedList = {
+  val: 1,
+  next: {
+    val: 2,
+    next: {
+      val: 3,
+      next: {
+        val: 4,
+        next: null,
+      },
+    },
+  },
+};
+
+const head = [1,2,3,4,5]
+
+console.log(reverseList(linkedList))
