85fe0175c7487baf75d3e990a312e935bf3b1556
Lance Yu <lanceyu619@gmail.com>
2023-05-12 12:48:21 -0400

    validparenthesis.js

---
+++ validParenthesis.js
@@ -0,0 +1,35 @@
+var isValid = function (s) {
+  let openingBrackets = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
+      openingBrackets.push(s[i]);
+    }
+    if (s[i] === ")" || s[i] === "]" || s[i] === "}") {
+      let iftrue;
+      let opening = openingBrackets.pop();
+      if (s[i] === ")") {
+        iftrue = "(";
+      }
+      if (s[i] === "]") {
+        iftrue = "[";
+      }
+      if (s[i] === "}") {
+        iftrue = "{";
+      }
+      console.log(openingBrackets);
+
+      if (opening !== iftrue) {
+        return false;
+      }
+    }
+  }
+  if (openingBrackets.length !==0){
+    return false
+  }
+  return true;
+};
+
+console.log(isValid("()"));
+console.log(isValid("([]){}"));
+console.log(isValid("()[]{}"));
+console.log(isValid("(("));
