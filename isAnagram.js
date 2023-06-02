01bed631d90268cf6efebd4888bb274f7e85322b
Lance Yu <lanceyu619@gmail.com>
2023-06-02 01:27:36 -0400

    group anagrams

--- isAnagram.js
+++ isAnagram.js
@@ -1,16 +1,27 @@
+// var isAnagram = function (s, t) {
+//   if (s.length !== t.length) {
+//     return false;
+//   }
+//   let sArr = s.split("");
+//   let tArr = t.split("");
+//   for (i = 0; i < tArr.length; i++) {
+//     if (!sArr.includes(tArr[i])) {
+//       return false;
+//     } else {
+//       let index = sArr.indexOf(tArr[i])
+//       sArr.splice(index,1)
+//     }
+//   }
+//   return true;
+// };
+
 var isAnagram = function (s, t) {
   if (s.length !== t.length) {
     return false;
   }
-  let sArr = s.split("");
-  let tArr = t.split("");
-  for (i = 0; i < tArr.length; i++) {
-    if (!sArr.includes(tArr[i])) {
-      return false;
-    } else {
-      let index = sArr.indexOf(tArr[i])
-      sArr.splice(index,1)
-    }
-  }
-  return true;
+  sortedSWord = s.split('').sort().join('')
+  sortedTWord = t.split('').sort().join('')
+  return (sortedSWord === sortedTWord )
 };
+
+console.log(isAnagram("abe","bae"))
