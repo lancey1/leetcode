3e4a5f151bbec2fd791633ed9d9d997487efbdb0
Lance Yu <lanceyu619@gmail.com>
2023-07-08 21:17:00 -0400

    jumpgame

--- jumpGame.js
+++ jumpGame.js
@@ -1,35 +1,22 @@
-function jump(nums) {
-  for (let i = 0; i < nums.length; i++) {
-    let jumpSteps = nums[i];
-    listofSteps = stepsThatCanBeTaken(jumpSteps);
-    listofStepslength = listofSteps.length
-    highestJump =  findHighestAvailJump(listofSteps,nums,listofStepslength)
-    console.log(highestJump)
+var jump = function (nums) {
+  let [left, right, jumps] = [0, 0, 0];
+
+  while (right < nums.length - 1) {
+    const maxReach = getMaxReach(nums, left, right);
+
+    left = right + 1;
+    right = maxReach;
+    jumps += 1;
   }
-}
 
-function stepsThatCanBeTaken(num) {
-  availStepsArr = [];
-  for (let j = 1; j <= num; j++) {
-    availStepsArr.push(j);
+  return jumps;
+};
+
+const getMaxReach = (nums, left, right, maxReach = 0) => {
+  for (let i = left; i < right + 1; i++) {
+    const reach = nums[i] + i;
+    maxReach = Math.max(maxReach, reach);
   }
-  return availStepsArr;
-}
 
-function findHighestAvailJump(stepsArr, nums,lengthofArr) {
-  jumpArr = [];
-  for (let i= stepsArr[0]; i < lengthofArr; i++) {
-    jumpArr.push(nums[i])
-  }
-  return Math.max(jumpArr);
-}
-
-
-// let listofSteps = [1,2]
-// let lengthofStepsArr = listofSteps.length
-nums = [2, 3, 1, 1, 4];
-
-// console.log(findHighestAvailJump(listofSteps,nums,lengthofStepsArr))
-
-console.log(jump(nums));
-s
+  return maxReach;
+};
